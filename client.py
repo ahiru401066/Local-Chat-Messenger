71c7b3f3ca80b4f7821225558bb4d18571e4938b
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-01-22 00:35:27 +0900

    20250122作業終了

--- client.py
+++ client.py
@@ -5,11 +5,11 @@ sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 
 # サーバのアドレスを定義します。
 # サーバはこのアドレスでメッセージを待ち受けます
-server_address = '/udp_socket_file'
+server_address = './udp_socket_file'
 
 # このクライアントのアドレスを定義します。
 # サーバはこのアドレスにメッセージを返します
-address = '/udp_client_socket_file'
+address = './udp_client_socket_file'
 
 # サーバに送信するメッセージを定義します
 message = b'Message to send to the client.'
